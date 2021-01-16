c2d1a1e6942be6d8d550ffa84d26fbf77f118379
ernestchu <b073040018@student.nsysu.edu.tw>
2021-01-16 10:23:58 +0800

    add data

--- src/get_img.py
+++ src/get_img.py
@@ -1,8 +1,11 @@
 import requests
+import time
 
-dir = '../dataset/'
+dir = 'training/dataset/'
 
-for i in range(25):
+for i in range(100):
+    time.sleep(1)
     img = requests.get('https://selcrs.nsysu.edu.tw/menu1/validcode.asp').content
     with open(dir+str(i)+'.bmp', 'wb') as f:
         f.write(img)
+    print('Get '+str(i))
